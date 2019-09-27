bb4b090f9df0d605a6352a5bb252e91a592f1023
suminhong02 <sumin.hong@ssfs.org>
2019-09-27 12:51:20 -0400

    Working on PointsAndRectangles

---
+++ src/PointsAndRectangles.java
@@ -0,0 +1,19 @@
+import java.awt.Rectangle;
+import java.awt.Point;
+import java.awt.Dimension;
+import java.util.Scanner;
+
+public class PointsAndRectangles {
+
+    public static void main(String []args){
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the x coordinate:");
+        int x = scan.nextInt();
+        System.out.print("Enter the y coordinate:");
+        int y = scan.nextInt();
+
+
+
+    }
+}
