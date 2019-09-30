603d6cbad3335e812638f0995094774e2f1c0c85
suminhong02 <sumin.hong@ssfs.org>
2019-09-30 15:02:16 -0400

    Completed PointsAndRectangles

--- src/PointsAndRectangles.java
+++ src/PointsAndRectangles.java
@@ -8,10 +8,22 @@ public class PointsAndRectangles {
     public static void main(String []args){
 
         Scanner scan = new Scanner(System.in);
-        System.out.print("Enter the x coordinate:");
+        System.out.print("Enter the x coordinate: ");
         int x = scan.nextInt();
-        System.out.print("Enter the y coordinate:");
+        System.out.print("Enter the y coordinate: ");
         int y = scan.nextInt();
+        System.out.print("Enter the length: ");
+        int length = scan.nextInt();
+        System.out.print("Enter the width: ");
+        int width = scan.nextInt();
+        Point p = new Point(x, y);
+        System.out.println(p);
+        Dimension d = new Dimension(length, width);
+        System.out.println(d);
+        Rectangle r = new Rectangle(x, y, length, width);
+        System.out.println(r);
+
+
 
 
 
