7e2ea37b0f53de171405cd6cf363ba314c44baa4
suminhong02 <sumin.hong@ssfs.org>
2019-10-03 10:06:15 -0400

    Completed FightSong

---
+++ src/FightSong.java
@@ -0,0 +1,31 @@
+public class FightSong {
+
+    public static void team(){
+        System.out.println("Go, team, go!");
+        System.out.println("You can do it.");
+    }
+
+    public static void west(){
+        System.out.println("You're the best,");
+        System.out.println("In the West.");
+    }
+
+    public static void blank_line(){
+        System.out.println(" ");
+    }
+
+    public static void main(String[] args){
+        team();
+        blank_line();
+        team();
+        west();
+        team();
+        blank_line();
+        team();
+        west();
+        team();
+        blank_line();
+        team();
+    }
+
+}
