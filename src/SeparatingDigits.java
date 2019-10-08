cbb153c5d544e98c7c86c450d28d7d3abff19d0d
suminhong02 <sumin.hong@ssfs.org>
2019-10-08 11:55:00 -0400

    Completed Separating Digits

--- src/SeparatingDigits.java
+++ src/SeparatingDigits.java
@@ -1,7 +1,33 @@
+import java.util.Scanner;
+
 public class SeparatingDigits {
 
-    public static void digits(String number){
-        System.out.println()
+    public static int quotient(int number) {
+        number = number/10;
+        return number;
+
+    }
+
+    public static int remainder(int number2) {
+        int number = number2%10;
+        return number;
+
+    }
+    public static void main(String[]args){
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter a four digit number:");
+        int four = scan.nextInt();
+        int fourth = remainder(four);
+        int three = quotient(four);
+        int third = remainder(three);
+        int two = quotient(three);
+        int second = remainder(two);
+        int first = quotient(two);
+
+        System.out.println(first+" "+second+" "+third+" "+fourth);
+
+
 
 
     }
