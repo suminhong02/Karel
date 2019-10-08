775085d1541542bdf0bda5b6088a478cf81cdcdc
suminhong02 <sumin.hong@ssfs.org>
2019-10-08 12:06:12 -0400

    Working on Names

---
+++ src/Names.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Names {
+    public static void main(String[]args){
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter first name ==>");
+        String first_name = scan.next();
+        System.out.print("Enter last name ==>");
+        String last_name = scan.next();
+
+    }
+}
