67f3e798f9eaae30e62ad25aa69e6954bf67321d
suminhong02 <hongsu@ssfs.org>
2019-10-25 17:09:00 -0400

    Completed Courses

--- src/Courses.java
+++ src/Courses.java
@@ -2,6 +2,18 @@ import java.util.Scanner;
 
 public class Courses {
     public static void main(String []args){
+        System.out.print("What is the description of the course?");
+        Scanner scan = new Scanner(System.in);
+        String description = scan.nextLine();
+
+        int first = description.indexOf(" ");
+        String department = description.substring(0,first);
+        System.out.println("Department: "+department);
+        int second = description.indexOf(" ",first+1);
+        String course_number = description.substring(first ,second);
+        System.out.println("Course Number: "+course_number);
+        String title = description.substring(second);
+        System.out.println("Title: "+title);
 
 
     }
